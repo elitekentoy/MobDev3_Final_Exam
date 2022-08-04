7aae9c19d7824d9e7f31d584ec3660e59cc86f76
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-08-05 02:21:26 +0800

    Wrap runApp with HydratedBlocOverrides

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,25 @@ import 'package:bloc_finals_exam/logic/bloc/bloc/tasks_bloc.dart';
 import 'package:bloc_finals_exam/models/task.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app_router.dart';
 import 'app_themes.dart';
 import 'screens/tabs_screen.dart';
 
-void main() {
-  runApp(
-    BlocOverrides.runZoned(() => MyApp(appRouter: AppRouter())),
-  );
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = HydratedStorage.build(storageDirectory: await getApplicationDocumentsDirectory());
+  // runApp(
+  //   HydratedBlocOverrides.runZoned(() => MyApp(appRouter: AppRouter()), storage: await storage),
+  // );
+  HydratedBlocOverrides.runZoned(
+  () => runApp(MyApp(appRouter: AppRouter(),)),
+  storage: await storage,
+);
+
 }
 
 class MyApp extends StatelessWidget {
