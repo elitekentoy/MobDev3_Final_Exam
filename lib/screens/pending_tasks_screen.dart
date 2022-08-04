ad9bb842f2b0d81df88db064cb94e5e1f694dd02
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-08-04 16:33:14 +0800

    Wrap PendingTasksScreens with BlocBuilder

--- lib/screens/pending_tasks_screen.dart
+++ lib/screens/pending_tasks_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../logic/bloc/bloc/tasks_bloc.dart';
+import '../models/task.dart';
 import '../test_data.dart';
 import '../widgets/tasks_list.dart';
 
@@ -8,22 +11,26 @@ class PendingTasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Chip(
-              label: Text(
-                '${TestData.pendingTasks.length} Pending | ${TestData.completedTasks.length} Completed',
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Chip(
+                  label: Text(
+                    '${TestData.pendingTasks.length} Pending | ${TestData.completedTasks.length} Completed',
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(height: 10),
+              TasksList(tasksList: TestData.pendingTasks),
+            ],
           ),
-          const SizedBox(height: 10),
-          TasksList(tasksList: TestData.pendingTasks),
-        ],
-      ),
+        );
+      },
     );
   }
 }
