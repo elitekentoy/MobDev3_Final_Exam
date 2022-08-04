b90545151ee65e63513d7dd9ba287edb8aeef760
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-08-04 18:43:00 +0800

    Implement updateTask on ListTile

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_finals_exam/logic/bloc/bloc/tasks_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
 import '../models/task.dart';
@@ -66,7 +68,9 @@ class TaskTile extends StatelessWidget {
           children: [
             Checkbox(
                 value: task.isDone,
-                onChanged: task.isDeleted! ? null : (value) {}),
+                onChanged: task.isDeleted! ? null : (value) {
+                  context.read<TasksBloc>().add(UpdateTask(task: task));
+                }),
             PopupMenu(
               task: task,
               editCallback: () {
