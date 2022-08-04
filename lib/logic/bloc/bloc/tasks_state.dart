1195b2e229892ed51020d3c806e55e0beed8f840
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-08-04 16:10:01 +0800

    Create Task List

--- lib/logic/bloc/bloc/tasks_state.dart
+++ lib/logic/bloc/bloc/tasks_state.dart
@@ -1,7 +1,12 @@
 part of 'tasks_bloc.dart';
 
-abstract class TasksState extends Equatable {
-  const TasksState();
+class TasksState extends Equatable {
+
+  final List<Task> taskList;
+
+  const TasksState(
+    {this.taskList = const <Task> []}
+  );
   
   @override
   List<Object> get props => [];
