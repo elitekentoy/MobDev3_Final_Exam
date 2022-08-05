034595c2d32117048f19f9db2bd272c9aeec7241
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-08-05 15:08:23 +0800

    Add fromMap on SwitchState

--- lib/logic/bloc/switch_bloc/switch_state.dart
+++ lib/logic/bloc/switch_bloc/switch_state.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 part of 'switch_bloc.dart';
 
- class SwitchState extends Equatable {
+class SwitchState extends Equatable {
   final bool switchValue;
 
 
@@ -8,6 +10,19 @@ part of 'switch_bloc.dart';
   
   @override
   List<Object> get props => [switchValue];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'switchValue': switchValue,
+    };
+  }
+
+  factory SwitchState.fromMap(Map<String, dynamic> map) {
+    return SwitchState(
+      switchValue: map['switchValue'] as bool,
+    );
+  }
+
 }
 
 class SwitchInitial extends SwitchState {
