c657f56219c0eb09466ffe33459f7b255afb0bc2
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-08-06 11:18:58 +0800

    Add RestoreTask class

--- lib/logic/bloc/tasks_bloc/tasks_event.dart
+++ lib/logic/bloc/tasks_bloc/tasks_event.dart
@@ -69,3 +69,14 @@ class EditTask extends TasksEvent {
     @override
   List<Object> get props => [oldTask, newTask];
 }
+
+class RestoreTask extends TasksEvent {
+  final Task task;
+
+  const RestoreTask({
+    required this.task,
+  });
+
+    @override
+  List<Object> get props => [task];
+}
